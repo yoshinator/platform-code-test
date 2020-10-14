ca15d092826a43c3586ab192728b6a38f6cc7233
Yoan Ante <yoanante@gmail.com>
2020-10-14 17:15:51 -0400

    moved logic to award class made each award a method

--- award.rb
+++ award.rb
@@ -8,8 +8,49 @@ class Award
   end
 
   def calculate_quality
-    self.expires_in >= 5 ? self.quality -= 2 : self.quality -= 4
-    self.quality < 0 && self.quality = 0
-    self.expires_in -= 1
+    if @name == 'NORMAL ITEM'
+      normal_item
+    elsif @name == 'Blue First'
+      blue_first
+    elsif @name == 'Blue Distinction Plus'
+      blue_distinction_plus
+    elsif @name == 'Blue Compare'
+      blue_compare
+    elsif @name == 'Blue Star'
+      blue_star
+    end 
+
+    @quality < 0 && @quality = 0
+    @quality > 50 && @quality = 50 unless @name == 'Blue Distinction Plus'
+    @expires_in -= 1 unless @name == 'Blue Distinction Plus'
   end
+
+  def normal_item
+    @expires_in > 0 ? @quality -= 1 : @quality -= 2
+  end
+
+  def blue_first
+    @expires_in > 0 ? @quality += 1 : @quality += 2
+  end
+
+  def blue_distinction_plus
+    @quality = 80
+  end
+
+  def blue_compare
+    if @expires_in < 1 
+      @quality = 0
+    elsif @expires_in < 6
+      @quality += 3
+    elsif @expires_in < 11
+      @quality += 2
+    else 
+      @quality += 1
+    end
+  end
+
+  def blue_star
+    @expires_in >= 5 ? @quality -= 2 : @quality -= 4
+  end
+
 end
